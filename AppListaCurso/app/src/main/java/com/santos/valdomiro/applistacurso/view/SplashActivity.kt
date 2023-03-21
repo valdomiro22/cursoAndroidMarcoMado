7f1842b842e262029167c972b7113a1d6edf8cf3
valdomiro santos <valdomiro.jose@outlook.com>
2023-03-21 16:41:42 -0300

    Troca de Tela com Handler e Delayed

--- AppListaCurso/app/src/main/java/com/santos/valdomiro/applistacurso/view/SplashActivity.kt
+++ AppListaCurso/app/src/main/java/com/santos/valdomiro/applistacurso/view/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.santos.valdomiro.applistacurso.view
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.santos.valdomiro.applistacurso.R
 import com.santos.valdomiro.applistacurso.databinding.ActivitySplashBinding
 
@@ -11,9 +14,21 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
 
+    val timeOutSplash: Int = 2000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        comutarTelaSplash()
+    }
+
+    private fun comutarTelaSplash() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
